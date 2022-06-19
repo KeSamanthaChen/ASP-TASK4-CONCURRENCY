fe25bcb706d3d26eca9ec3ccc3996ade505d0c3b
KeSamanthaChen <ke.samantha.chen@tum.de>
2022-06-19 17:05:07 +0000

    test the 11 problem

--- lockhashmap.cpp
+++ lockhashmap.cpp
@@ -39,79 +39,80 @@ typedef struct hm_t {
 // allocate a hashmap with given number of buckets
 // https://stackoverflow.com/questions/39397322/declaring-a-2d-array-using-double-pointer
 HM* alloc_hashmap(size_t n_buckets) {
-    HM new_hm;
-    new_hm.n_buckets = n_buckets;
-    new_hm.buckets = new List*[n_buckets]; // need delete
-    for (int i=0; i<n_buckets; i++) {
-        // do some thing
-        new_hm.buckets[i]->sentinel = new Node_HM_t;
-        new_hm.buckets[i]->sentinel->m_val = i; // kind of like the key
-    }
-    return &new_hm;
+    return NULL;
+    // HM new_hm;
+    // new_hm.n_buckets = n_buckets;
+    // new_hm.buckets = new List*[n_buckets]; // need delete
+    // for (int i=0; i<n_buckets; i++) {
+    //     // do some thing
+    //     new_hm.buckets[i]->sentinel = new Node_HM_t;
+    //     new_hm.buckets[i]->sentinel->m_val = i; // kind of like the key
+    // }
+    // return &new_hm;
 }
 
 //free a hashamp
 void free_hashmap(HM* hm) {
-    for (int i=0; i<hm->n_buckets; i++) {
-        // delete sentinel is different from delete hm->buckets[i]
-        Node_HM* current_node = hm->buckets[i]->sentinel;
-        Node_HM* pointer;
-        while(current_node != NULL) {
-            // when insert value, this will be using "new", so need to delete there
-            pointer = current_node;
-            current_node = current_node->m_next;
-            delete pointer;
-        }
-        delete hm->buckets[i];
-    }
-    delete hm->buckets;
+    // for (int i=0; i<hm->n_buckets; i++) {
+    //     // delete sentinel is different from delete hm->buckets[i]
+    //     Node_HM* current_node = hm->buckets[i]->sentinel;
+    //     Node_HM* pointer;
+    //     while(current_node != NULL) {
+    //         // when insert value, this will be using "new", so need to delete there
+    //         pointer = current_node;
+    //         current_node = current_node->m_next;
+    //         delete pointer;
+    //     }
+    //     delete hm->buckets[i];
+    // }
+    // delete hm->buckets;
 }
 
 //insert val into the hm and return 0 if successful
 //return 1 otherwise, e.g., could not allocate memory
 int insert_item(HM* hm, long val) {
-    long key_value = val%hm->n_buckets;
-    //if the value already here? duplicated allow
-    Node_HM* current_node = hm->buckets[key_value]->sentinel;
-    while (current_node->m_next!=NULL) {
-        current_node = current_node->m_next;
-    }
-    //end of list
-    Node_HM* new_node = new Node_HM;
-    new_node->m_next = NULL;
-    new_node->m_val = val;
-    current_node->m_next = new_node;
+    // long key_value = val%hm->n_buckets;
+    // //if the value already here? duplicated allow
+    // Node_HM* current_node = hm->buckets[key_value]->sentinel;
+    // while (current_node->m_next!=NULL) {
+    //     current_node = current_node->m_next;
+    // }
+    // //end of list
+    // Node_HM* new_node = new Node_HM;
+    // new_node->m_next = NULL;
+    // new_node->m_val = val;
+    // current_node->m_next = new_node;
     return 0;
 }
 
 //remove val from the hm, if it exist and return 0 if successful
 //return 1 if item is not found
 int remove_item(HM* hm, long val) {
-    long key_value = val%hm->n_buckets;
-    Node_HM* current_node = hm->buckets[key_value]->sentinel->m_next;
-    Node_HM* previous_node = hm->buckets[key_value]->sentinel;
-    while (current_node!=NULL) {
-        if (current_node->m_val == val) {
-            // delete and reconnect
-            previous_node->m_next = current_node->m_next;
-            delete current_node;
-            return 0;
-        }
-        // looping update
-        previous_node = current_node;
-        current_node = current_node->m_next;
-    }
+    // long key_value = val%hm->n_buckets;
+    // Node_HM* current_node = hm->buckets[key_value]->sentinel->m_next;
+    // Node_HM* previous_node = hm->buckets[key_value]->sentinel;
+    // while (current_node!=NULL) {
+    //     if (current_node->m_val == val) {
+    //         // delete and reconnect
+    //         previous_node->m_next = current_node->m_next;
+    //         delete current_node;
+    //         return 0;
+    //     }
+    //     // looping update
+    //     previous_node = current_node;
+    //     current_node = current_node->m_next;
+    // }
     return 1;
 }
 
 //check if val exists in hm, return 0 if found, return 1 otherwise
 int lookup_item(HM* hm, long val) {
-    long key_value = val%hm->n_buckets;
-    Node_HM* current_node = hm->buckets[key_value]->sentinel->m_next;
-    while (current_node!=NULL) {
-        if (current_node->m_val == val)
-            return 0;
-    }
+    // long key_value = val%hm->n_buckets;
+    // Node_HM* current_node = hm->buckets[key_value]->sentinel->m_next;
+    // while (current_node!=NULL) {
+    //     if (current_node->m_val == val)
+    //         return 0;
+    // }
     return 1;
 }
 
@@ -120,12 +121,12 @@ int lookup_item(HM* hm, long val) {
 //Bucket 2 - val4 - val5 - val6 ...
 //Bucket N -  ...
 void print_hashmap(HM* hm) {
-    for(int i=0;i<hm->n_buckets;i++) {
-        std::cout << "Bucket " << i << std::flush;
-        Node_HM* current_node = hm->buckets[i]->sentinel->m_next;
-        while(current_node!=NULL) {
-            std::cout << " - " << current_node->m_val << std::flush;
-        }
-        std::cout << std::endl;
-    }
+    // for(int i=0;i<hm->n_buckets;i++) {
+    //     std::cout << "Bucket " << i << std::flush;
+    //     Node_HM* current_node = hm->buckets[i]->sentinel->m_next;
+    //     while(current_node!=NULL) {
+    //         std::cout << " - " << current_node->m_val << std::flush;
+    //     }
+    //     std::cout << std::endl;
+    // }
 }
