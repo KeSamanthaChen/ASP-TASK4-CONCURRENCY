df876ace432b7ab8af7a9de6e97366f56b3b60b4
KeSamanthaChen <ke.samantha.chen@tum.de>
2022-06-21 20:29:35 +0000

    change the header to remove atomic

--- chashmap.h
+++ chashmap.h
@@ -4,7 +4,7 @@
 #define PAD 64
 
 #ifdef __cplusplus
-#include <atomic>
+// #include <atomic>
 #define EXTERNC extern "C"
 #else
 #define EXTERNC
@@ -20,7 +20,7 @@ EXTERNC int insert_item(HM* hm, long val);
 EXTERNC int remove_item(HM* hm, long val);
 EXTERNC int lookup_item(HM* hm, long val);
 EXTERNC void print_hashmap(HM* hm);
-EXTERNC Node_HM* search_item(HM* hm, long val, std::atomic<Node_HM*>* left_node);
+EXTERNC Node_HM* search_item(HM* hm, long val, Node_HM** left_node);
 
 EXTERNC bool is_marked_reference(void *i);
 EXTERNC Node_HM *get_unmarked_reference(void *w);
